5c50901e7e8b37de6997d7352fda8e606fa19f6a
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-05 10:35:00 +0200

    Hello nev

--- 1-vezerlesi-szerkezetek/1-alapismeretek/BeolvasKiir/Program.cs
+++ 1-vezerlesi-szerkezetek/1-alapismeretek/BeolvasKiir/Program.cs
@@ -6,7 +6,13 @@ namespace BeolvasKiir
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Világ!");
+            // 0. Írjuk ki, hogy "Hello Világ!"
+            // Console.WriteLine("Hello Világ!");
+
+            // 1. Köszöntsünk valakit a nevén!
+            string nev;
+            nev = Console.ReadLine();
+            Console.WriteLine("Szia " + nev + "!");
 
             Console.ReadKey();
         }

da089621676ab5066549a7aac01e2c531b2830aa
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-11 11:32:16 +0200

    1-alapok Idokonverzio

---
+++ 1-vezerlesi-szerkezetek/1-alapok/Idokonverzio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Idokonverzio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int ora = r.Next(24);
+            int perc = r.Next(60);
+            int mperc = r.Next(60);
+            Console.WriteLine($"Idő: {ora:00}:{perc:00}:{mperc:00}");
+
+            int masodperc = ora * 3600 + perc * 60 + mperc;
+            Console.WriteLine($"A nap eleje óta {masodperc} másodperc telt el.");
+
+            Console.ReadKey();
+        }
+    }
+}

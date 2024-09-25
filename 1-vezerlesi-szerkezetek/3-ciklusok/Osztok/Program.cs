729649e24c6d19935ccc0df19666ecf944a23a54
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-25 11:29:38 +0200

    Negyzetszam, Osztok

---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Osztok/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Osztok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

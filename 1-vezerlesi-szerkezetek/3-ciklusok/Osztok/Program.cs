6a66f223f5c270057e758cedf0d366e1625dfe0f
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-26 10:05:02 +0200

    Osztok: előfeltétel tesztelés (do-while)

--- 1-vezerlesi-szerkezetek/3-ciklusok/Osztok/Program.cs
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Osztok/Program.cs
@@ -6,8 +6,26 @@ namespace Osztok
     {
         static void Main(string[] args)
         {
+            int n;
+
+            // While előfeltétel tesztelés
+            /*
             Console.Write("n: ");
-            int n = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n <= 0)
+            {
+                Console.WriteLine("Nem jó! Pozitív egész számot adj!");
+                Console.Write("n: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            */
+
+            do
+            {
+                Console.WriteLine("Adj meg egy pozitív egész számot, amelynek az osztóit szeretnéd tudni!");
+                Console.Write("n: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n <= 0);
 
             for (int i = 1; i <= n; i++)
             {

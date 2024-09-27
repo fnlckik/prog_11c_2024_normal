fcd83a1515f4c5c90613f9d6db91f07aba2dafec
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-27 09:48:51 +0200

    Kocka (kesz), Erme a)

---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Erme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Erme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 50;
+            Random r = new Random();
+
+            // a) Dobjunk fel egy érmét 50-szer!
+            Console.WriteLine("Érme dobás 50-szer:");
+            int dbF = 0, dbI = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int dobas = r.Next(1, 3); //1..2
+                if (dobas == 1)
+                {
+                    Console.Write("F ");
+                    dbF++;
+                }
+                else
+                {
+                    Console.Write("I ");
+                    dbI++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Fejek száma: {dbF}");
+            Console.WriteLine($"Írások száma: {dbI}");
+
+            Console.ReadKey();
+        }
+    }
+}

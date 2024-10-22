38b55665a5a9a66574e306c4b5454eed726e14ec
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-22 11:30:13 +0200

    5. Kiválasztás, 6. Keresés

--- 2-programozasi-tetelek/2-alapfeladatok/Alapfeladatok2/Program.cs
+++ 2-programozasi-tetelek/2-alapfeladatok/Alapfeladatok2/Program.cs
@@ -7,10 +7,11 @@ namespace Alapfeladatok2
         static void Main(string[] args)
         {
             int[] x = { 5, 7, 2, 2, 9, -1, 4, -3, 9, 3, 2, 1 };
-            int n = x.Length;
+            int n = x.Length; // 12
 
             // 4. Eldöntés
             // Van-e benne negatív elem?
+            // !(x[i] < 0) helyett: x[i] >= 0
             int i = 0;
             while (i < n && !(x[i] < 0))
             {
@@ -19,11 +20,37 @@ namespace Alapfeladatok2
 
             if (i < n)
             {
-                Console.WriteLine("Van benne negatív elem!");
+                Console.WriteLine("4. Van benne negatív elem!");
             }
             else
             {
-                Console.WriteLine("Nincs benne negatív elem!");
+                Console.WriteLine("4. Nincs benne negatív elem!");
+            }
+
+            // 5. Kiválasztás => Nagyon ritkán használjuk!
+            // !(x[j]) == 9 helyett: x[j] != 9
+            int j = 0;
+            while (!(x[j] == 9))
+            {
+                j++;
+            }
+            Console.WriteLine($"5. Az első 9-es sorszáma: {j+1}");
+
+            // 6. Keresés (lineáris keresés)
+            int k = 0;
+            while (k < n && !(x[k] % 2 == 0))
+            {
+                k++;
+            }
+            if (k < n)
+            {
+                Console.WriteLine("6. Az első páros szám:");
+                Console.WriteLine($"   a) Sorszáma: {k+1}");
+                Console.WriteLine($"   b) Értéke: {x[k]}");
+            }
+            else
+            {
+                Console.WriteLine("6. Nincs a tömbben páros szám!");
             }
 
             Console.ReadKey();

636af5600c779b3fa972c15c12ad751268a802f9
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-13 10:30:55 +0100

    Reptér F3-F4

--- 2-programozasi-tetelek/3-repter/Repter/Program.cs
+++ 2-programozasi-tetelek/3-repter/Repter/Program.cs
@@ -10,6 +10,7 @@ namespace Repter
                                "Kiss Júlia", "Molnár Tamás", "Farkas Dóra", "Horváth István",
                                "Varga Krisztina", "Simon Balázs", "Lukács Zoltán", "Takács Emese" };
             double[] tomegek = { 22.3, 12, 7.5, 18.44, 35, 28, 14.12, 8, 16.9, 5.8, 30, 24 };
+            //double[] tomegek = { 22.3, 120, 70.5, 180.44, 35, 28, 140.12, 80, 160.9, 50.8, 30, 24 };
             // 12 elemünk van!
             int n = tomegek.Length;
 
@@ -44,8 +45,45 @@ namespace Repter
                 Console.WriteLine("2. Nincs ilyen!");
             }
 
-            // 3. Feltételes átlag = Összegzés + Megszámolás
+            // F3 - Feltételes átlag = Összegzés + Megszámolás
+            double osszeg = 0;
+            int db = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (tomegek[i] < 20)
+                {
+                    osszeg += tomegek[i];
+                    db++;
+                }
+            }
+            if (db == 0)
+            {
+                Console.WriteLine("3. Nincs 20 kg alatti csomag!");
+            }
+            else
+            {
+                double atlag = osszeg / db;
+                Console.WriteLine($"3. {atlag:0.00} kg");
+            }
+            // NaN: Not a Number, jellemzően 0/0 esetén fordul elő.
 
+            // F4 - Eldöntés (optimista eldöntés)
+            // Van-e 5-nél kisebb vagy egyenlő szám?
+            // Van => Nem igaz!
+            // Nincs => Igaz!
+            i = 0;
+            while (i < n && !(tomegek[i] <= 5))
+            {
+                i++;
+            }
+            if (i < n) // Megtaláltam a keresett elemet: van 5-nél kisebb vagy egyenlő
+            {
+                Console.WriteLine("4. Nem igaz, hogy mindegyik 5-nél nagyobb tömegű!");
+            }
+            else
+            {
+                Console.WriteLine("4. Igaz, hogy mindegyik 5-nél nagyobb tömegű!");
+            }
 
             Console.ReadKey();
         }

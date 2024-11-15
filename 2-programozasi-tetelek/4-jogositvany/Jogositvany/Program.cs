275590761630ffa687292097dd4ab6ff621a3fa2
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-15 10:39:24 +0100

    Jogositvany F1 - Console.ReadKey() hiba

---
+++ 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Jogositvany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Statikus tömbök: fordítási időben tudnunk kell a méretét!
+            // EZ ÍGY NEM JÓ!!!
+            // string[] nevek = new string[n];
+            string[] nevek = new string[100];
+            int[] korok = new int[100];
+            bool[] jogsik = new bool[100]; // jogsik[i]: az i. adatnak van-e jogsija?
+
+            // F1 - Beolvasás
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                string sor = Console.ReadLine();
+                Console.WriteLine(sor);
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}

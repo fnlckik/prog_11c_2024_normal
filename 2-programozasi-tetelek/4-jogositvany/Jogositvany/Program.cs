1ec397d85cea60d999dc8bac75739b35bc271716
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-19 10:35:44 +0100

    4-jogositvany F1 (beolvasas)

--- 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
+++ 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
@@ -6,6 +6,10 @@ namespace Jogositvany
     {
         static void Main(string[] args)
         {
+            // Elnevezési konveciók:
+            // PascalCase => C# (ReadLine)
+            // camelCase => JS (addEventListener)
+            // kebab-case => CSS (list-style-type)
             int n = Convert.ToInt32(Console.ReadLine());
 
             // Statikus tömbök: fordítási időben tudnunk kell a méretét!
@@ -16,11 +20,19 @@ namespace Jogositvany
             bool[] jogsik = new bool[100]; // jogsik[i]: az i. adatnak van-e jogsija?
 
             // F1 - Beolvasás
+            /*
+             if (adatok[2] == "I") jogsik[i] = true;
+             else jogsik[i] = false; 
+            */
+            // jogsik[i] = adatok[2] == "I" ? true : false; // ternary operator
             int i;
             for (i = 0; i < n; i++)
             {
-                string sor = Console.ReadLine();
-                Console.WriteLine(sor);
+                string sor = Console.ReadLine(); // "Daniel 30 I"
+                string[] adatok = sor.Split(' '); // { "Daniel", "30", "I" }
+                nevek[i] = adatok[0]; // { "Daniel", "Marta", ... }
+                korok[i] = Convert.ToInt32(adatok[1]);
+                jogsik[i] = adatok[2] == "I";
             }
 
             //Console.ReadKey();

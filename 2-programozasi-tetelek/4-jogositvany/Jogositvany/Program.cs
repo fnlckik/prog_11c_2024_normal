3a943a026618471bb4e09fbfd8385e9b584bc7bd
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-24 15:34:12 +0100

    4-Jogositvany F6-F7

--- 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
+++ 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
@@ -122,10 +122,53 @@ namespace Jogositvany
             }
 
             // F6 - Keresés -> while (nem járjuk be az egész tömböt)
-
+            i = 0;
+            // nev = nevek[0] -> "Daniel"
+            // nev[0] -> 'D'
+            // nevek[0][0] -> 'D'
+            while (i < n && !(nevek[i][0] == 'F'))
+            {
+                i++;
+            }
+            if (i < n)
+            {
+                Console.WriteLine($"6. {nevek[i]}");
+            }
+            else
+            {
+                Console.WriteLine($"6. Nincs F betűvel kezdődő név.");
+            }
 
             // F7 - Összegzés
+            int osszeg = 0;
+            // utolsó (hátulról az 1.): n-1
+            // utolsó előtti (hátulról a 2.): n-2
+            // ...
+            // hátulról a 10.: n-10
+            for (i = n-10; i < n; i++)
+            {
+                osszeg += korok[i];
+            }
+            double atlag = (double) osszeg / 10;
+            Console.WriteLine($"7. {atlag}");
 
+            // MO2
+            int osszeg2 = 0;
+            for (i = n-1; i >= n-10; i--)
+            {
+                osszeg2 += korok[i];
+            }
+            double atlag2 = (double)osszeg2 / 10;
+            Console.WriteLine($"7. {atlag2}");
+
+            // MO2
+            int osszeg3 = 0;
+            for (i = 1; i <= 10; i++)
+            {
+                osszeg3 += korok[n-i]; // n-1, n-2, ..., n-10
+            }
+            double atlag3 = (double)osszeg3 / 10;
+            Console.WriteLine($"7. {atlag3}");
 
             //Console.ReadKey();
         }

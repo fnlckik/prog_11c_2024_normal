ab8991b1a5f24bc2030fbeb1562524476a702510
fnlckik <farkas.norbert@ckik.hu>
2024-11-29 10:09:21 +0100

    3-kivalogatas Draga_fenyofak

---
+++ Mester/Kezdo/3-Kivalogatas-5-Draga_fenyofak/DragaFenyofak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DragaFenyofak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklaráció
+            int n, k;
+            int[] arak = new int[100];
+
+            // Beolvasás
+
+
+            // Feldolgozás - Kiválogatás
+
+
+            // Kiírás
+
+
+        }
+    }
+}

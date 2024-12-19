1589c0905dc34c76609529f36c80ee699ef08791
farkas.norbert <farkas.norbert@ckik.hu>
2024-12-19 10:14:12 +0100

    Mátrix - F3 összegzés

--- 4-matrix/Matrix/Program.cs
+++ 4-matrix/Matrix/Program.cs
@@ -16,19 +16,43 @@ namespace Matrix
             // matrix[0, 0]
 
             // F1
+            Console.WriteLine("1. feladat:");
             Console.WriteLine(matrix[1, 2]); // 2. sor 3. eleme
             Console.WriteLine($"Elemek száma: {matrix.Length}");
             Console.WriteLine($"Sorok száma: {matrix.GetLength(0)}"); // dim1
             Console.WriteLine($"Oszlopok száma: {matrix.GetLength(1)}"); // dim2
 
             // F2
-            for (int i = 0; i < matrix.GetLength(0); i++) // sorok bejárása
+            Console.WriteLine("\n2. feladat:");
+            int n = matrix.GetLength(0); // sorok száma
+            int m = matrix.GetLength(1); // oszlopok száma
+            for (int i = 0; i < n; i++) // sorok bejárása
             {
-                for (int j = 0; j < matrix.GetLength(1); j++) // elemek bejárása
+                for (int j = 0; j < m; j++) // elemek bejárása
                 {
-                    Console.WriteLine(matrix[i, j]); // i. sor j. eleme
+                    if (j == m-1 && i == n-1)
+                    {
+                        Console.Write(matrix[i, j]); // i. sor j. eleme
+                    }
+                    else
+                    {
+                        Console.Write(matrix[i, j] + ", "); // i. sor j. eleme
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // F3
+            int osszeg = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    osszeg += matrix[i, j];
                 }
             }
+            Console.WriteLine($"\n3. feladat: {osszeg}");
+
         }
     }
 }

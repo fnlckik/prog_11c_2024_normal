2350abab56f3e2c31893516997c0fae95f7902b4
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-10 14:20:38 +0100

    MelegebbTelepules kesz

--- Mester/Kozephalado/3-Idojaras-23-MelegebbTelep/MelegebbTelepules/Program.cs
+++ Mester/Kozephalado/3-Idojaras-23-MelegebbTelep/MelegebbTelepules/Program.cs
@@ -13,7 +13,84 @@ namespace MelegebbTelepules
             // Keressük meg az első 18-nál nagyobb hőmérsékletet!
             //Console.WriteLine(Keres18Nagyobb(h, n, m)); // 20
 
+            // Írjuk ki az (a, b) számpárokat, ahol a<b. k-ig tegyük meg!
+            // (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)
+            /*
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = i+1; j <= 4; j++)
+                {
+                    Console.Write($"({i}, {j})\t");
+                }
+            }
+            */
 
+            // Írjuk ki az (a, b) számpárokat, ahol a<b. k-ig tegyük meg!
+            // (1, 1), (1, 2), (1, 3), (1, 4), (2, 1), (2, 2), (2, 3), ...
+            /*
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 4; j++)
+                {
+                    Console.Write($"({i}, {j})\t");
+                }
+            }
+            */
+
+            // i. sor VS j. sor
+            /*
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (MelegebbE(h, m, i, j))
+                    {
+                        Console.WriteLine(i+1);
+                    }
+                    //Console.WriteLine($"{i+1} {j+1} {MelegebbE(h, m, i, j)}\t");
+                }
+            }
+            */
+
+            // Keressünk olyan i. települést, amely melegebb valamely j. településnél!
+            int i = MasnalMelegebb(h, n, m);
+            Console.WriteLine(i);
+        }
+
+        static int MasnalMelegebb(int[,] h, int n, int m)
+        {
+            int i = 0;
+            int j = 0;
+            bool megvanE = false;
+            while (i < n && !megvanE)
+            {
+                j = 0;
+                while (j < n && !megvanE)
+                {
+                    megvanE = MelegebbE(h, m, i, j);
+                    j++;
+                }
+                i++;
+            }
+            if (megvanE)
+            {
+                return i; // 2
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        // Igaz-e, hogy az i. település melegebb a j. településnél?
+        static bool MelegebbE(int[,] h, int m, int i, int j)
+        {
+            int k = 0; // k. napot nézzük
+            while (k < m && (h[i, k] > h[j, k]))
+            {
+                k++;
+            }
+            return !(k < m);
         }
 
         static int Keres18Nagyobb(int[,] h, int n, int m)

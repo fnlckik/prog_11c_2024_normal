27271ea6c79071caf676d044278a3f36fab19c52
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-23 10:19:50 +0100

    Jegyek - Beolvas()

--- 5-sokasagok/Jegyek/Program.cs
+++ 5-sokasagok/Jegyek/Program.cs
@@ -7,6 +7,40 @@ namespace Jegyek
     {
         static void Main(string[] args)
         {
+            List<List<int>> tanulok = new List<List<int>>();
+            Beolvas(tanulok);
+            Kiir(tanulok);
+        }
+
+        /*
+            1. tanuló: 5 3 5 5 4
+            2. tanuló: 5 5 4 5
+            3. tanuló: 2 2 1 2 3
+            4. tanuló: 3 5 2
+            5. tanuló: 1 4 1 2 4 4 
+        */
+        static void Kiir(List<List<int>> tanulok)
+        {
+            // ???
+        }
+
+        static void Beolvas(List<List<int>> tanulok)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                List<int> tanulo = new List<int>(); // létrehoztunk egy üres listát
+                string[] sor = Console.ReadLine().Split(' '); // {"5", "3", "5", "5", "4"}
+                //for (int j = 0; j < sor.Length; j++)
+                //{
+                //    tanulo.Add(Convert.ToInt32(sor[j]));
+                //}
+                foreach (string jegy in sor)
+                {
+                    tanulo.Add(int.Parse(jegy));
+                }
+                tanulok.Add(tanulo);
+            }
         }
     }
 }

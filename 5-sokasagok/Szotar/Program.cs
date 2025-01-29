06b0f32756d9e51eb1b4a299b79281b571cec194
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-29 11:36:21 +0100

    Szotar - magyarAngol

---
+++ 5-sokasagok/Szotar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Szotar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // szótár ~ Dictionary ~ Map ~ asszociatív tömb (asszociáció = képzettársítás)
+            // szótár: kulcs-érték párokból áll (kulcs egyedi)
+            Dictionary<string, string> magyarAngol = new Dictionary<string, string>();
+            magyarAngol.Add("kutya", "dog");
+            magyarAngol.Add("macska", "cat");
+            magyarAngol.Add("oroszlán", "lion");
+
+            Console.WriteLine("Szópárok száma: " + magyarAngol.Count);
+
+            Console.WriteLine("Macska angolul: " + magyarAngol["macska"]);
+        }
+    }
+}

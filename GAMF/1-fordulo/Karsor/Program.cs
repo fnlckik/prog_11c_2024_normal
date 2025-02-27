fabaa1216586ab574e0c20befd8d408d1811a205
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-27 10:36:21 +0100

    Karsor - beolvas

---
+++ GAMF/1-fordulo/Karsor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Karsor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Beolvas();
+            Tavolsagok(s);
+        }
+
+        static void Tavolsagok(string s)
+        {
+            // Írd ki az aktuális pozíciókat!
+            // a = észak, b = kelet, c = dél, d = nyugat
+            // P(0;0) -> P(0;1) -> P(-1;1) -> P(-1;0) -> ...
+        }
+
+        static string Beolvas()
+        {
+            StreamReader reader = new StreamReader("../../../karsor.txt");
+            string s = reader.ReadLine();
+            reader.Close();
+            return s;
+        }
+    }
+}
---
+++ GAMF/1-fordulo/Karsor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Karsor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Karsor")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("31d9c725-7f7f-44f9-9e2c-ab8ee1779c0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

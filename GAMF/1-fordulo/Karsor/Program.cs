8f1190323fdbc30534471cd262d30f8851ecc6e5
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-28 09:20:45 +0100

    GAMF - Karsor

--- GAMF/1-fordulo/Karsor/Program.cs
+++ GAMF/1-fordulo/Karsor/Program.cs
@@ -16,6 +16,30 @@ namespace Karsor
             // Írd ki az aktuális pozíciókat!
             // a = észak, b = kelet, c = dél, d = nyugat
             // P(0;0) -> P(0;1) -> P(-1;1) -> P(-1;0) -> ...
+            int x = 0, y = 0; // Kezdő pozíció (origo)
+            StreamWriter writer = new StreamWriter("poziciok.txt");
+            writer.WriteLine($"P({x}; {y})"); // P(0; 0)
+            foreach (char c in s)
+            {
+                if (c == 'a')
+                {
+                    y++; // Észak
+                }
+                else if (c == 'b')
+                {
+                    x++; // Kelet
+                }
+                else if (c == 'c')
+                {
+                    y--; // Dél
+                }
+                else
+                {
+                    x--; // Nyugat
+                }
+                writer.WriteLine($"P({x}; {y})");
+            }
+            writer.Close();
         }
 
         static string Beolvas()

0590e29452d20d4c791caf0cd91c8130d8f00c33
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-12 11:32:30 +0100

    7-rekord Ember struct

---
+++ 7-rekord/Ember/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ember
+{
+    internal class Program
+    {
+        // Rekord: adatok egységbe zárása (struct)
+        // OOP: Objektum Orientált Programozás
+        // Készítsünk egy saját típust!
+        // Adattag: tulajdonság (attribútum)
+        struct Ember
+        {
+            public string nev;
+            public int kor;
+            public double magassag; // méterben
+        }
+
+        static void Main(string[] args)
+        {
+            //Random r = new Random();
+            //Stopwatch ora = new Stopwatch();
+            //List<int> lista = new List<int> { 2, 5, 1 };
+            //Int32 szam;
+            //String szoveg;
+
+            Ember bela; // objektum
+            bela.nev = "Béla";
+            bela.kor = 52;
+            bela.magassag = 1.4;
+
+            // példányosítás
+            Ember lajos = new Ember { nev = "Lajos", kor = 12, magassag = 1.23 };
+
+            Console.WriteLine($"Embereink nevei: {bela.nev}, {lajos.nev}");
+            //Console.WriteLine($"{bela.nev} vagyok, {bela.kor} éves.");
+            //Console.WriteLine($"{lajos.nev} vagyok, {lajos.kor} éves.");
+            Bemutatkozas(bela);
+            Bemutatkozas(lajos);
+        }
+
+        // Ember: struktúra
+        // ember: objektum
+        static void Bemutatkozas(Ember ember)
+        {
+            Console.WriteLine($"{ember.nev} vagyok, {ember.kor} éves.");
+        }
+    }
+}

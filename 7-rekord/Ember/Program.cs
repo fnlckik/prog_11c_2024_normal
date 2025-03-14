eb9c21196d7461e23749dc9e9e88854aeaf1d1aa
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-14 09:07:32 +0100

    Ember - Szulinap

--- 7-rekord/Ember/Program.cs
+++ 7-rekord/Ember/Program.cs
@@ -36,6 +36,61 @@ namespace Ember
             //Console.WriteLine($"{lajos.nev} vagyok, {lajos.kor} éves.");
             Bemutatkozas(bela);
             Bemutatkozas(lajos);
+
+            Ember frigyes = new Ember
+            {
+                nev = "Frigyes",
+                kor = 73,
+                magassag = 2.2
+            };
+            Bemutatkozas(frigyes);
+
+            // Igaz-e, hogy Béla alacsonyabb Lajosnál?
+            //Console.WriteLine("Béla alacsonyabb Lajosnál? " + (bela.magassag < lajos.magassag));
+            Console.WriteLine($"Béla alacsonyabb Lajosnál? {bela.magassag < lajos.magassag}");
+
+            Ember legmagasabb = Legmagasabb(bela, lajos, frigyes);
+            Console.WriteLine($"Legmagasabb ember: {legmagasabb.nev}");
+
+            Console.WriteLine($"Átlagos életkor: {Atlag(bela, lajos, frigyes)}");
+
+            Szulinap(ref bela);
+            Bemutatkozas(bela);
+        }
+
+        // Vigyázat! Érték szerinti paraméter átadásnál másolás történik!
+        // Új Ember objektum a memóriában!
+        // NODE! Referencia szerinti paraméter átadásnál már tudjuk módosítani!
+        static void Szulinap(ref Ember ember)
+        {
+            //ember.nev = "Robi";
+            ember.kor++;
+        }
+
+        static double Atlag(Ember e1, Ember e2, Ember e3)
+        {
+            double atlag = (double)(e1.kor + e2.kor + e3.kor) / 3;
+            return Math.Round(atlag, 2);
+        }
+
+        static Ember Legmagasabb(Ember e1, Ember e2, Ember e3)
+        {
+            Ember e = e1;
+            if (e2.magassag > e.magassag) e = e2;
+            if (e3.magassag > e.magassag) e = e3;
+            return e;
+            //if (e1.magassag >= e2.magassag && e1.magassag >= e3.magassag)
+            //{
+            //    return e1;
+            //}
+            //else if (e2.magassag >= e3.magassag)
+            //{
+            //    return e2;
+            //}
+            //else
+            //{
+            //    return e3;
+            //}
         }
 
         // Ember: struktúra

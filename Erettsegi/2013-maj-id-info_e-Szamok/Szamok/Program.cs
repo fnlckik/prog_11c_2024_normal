0a059880ad5752d7e76870246cb73475fadec79d
farkas.norbert <farkas.norbert@ckik.hu>
2025-04-08 11:31:03 +0200

    Szamok - Kerdes struktúra

---
+++ Erettsegi/2013-maj-id-info_e-Szamok/Szamok/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Szamok
+{
+    // long - Int64 (8 bájt), int - Int32 (4 bájt),
+    // short - Int16 (2 bájt), byte (1 bájt, 256 féle)
+    struct Kerdes
+    {
+        public string szoveg;
+        public int valasz;
+        public byte pontszam; // 1-től 3-ig
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kerdes> kerdesek = new List<Kerdes>();
+        }
+    }
+}

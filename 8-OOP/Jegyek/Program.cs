402773b8c3e048bcaeb9778968c25fff9f3f5330
farkas.norbert <farkas.norbert@ckik.hu>
2025-04-29 10:53:06 +0200

    8 - OOP

---
+++ 8-OOP/Jegyek/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+
+namespace Jegyek
+{
+    // Osztály: egységbe zárja az objektumok
+    // 1. Adattagjait (field) - mező
+    // 2. Műveleteit (method) - metódus
+    class Diak
+    {
+        public string nev;
+        public int kor;
+        public double hangulat; // 0.00-1.00 százalékos érték
+
+        public Diak() { }
+
+        public Diak(string nev, int kor, double hangulat)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        struct Tanulo
+        {
+            public string nev;
+            public int kor;
+            public double hangulat; // 0.00-1.00
+        }
+
+        // OOP: objektum orientált programozás
+        static void Main(string[] args)
+        {
+            //Korabban();
+
+            // Példányosítás: létrejön az osztályból egy konkrét objektum példány
+            // Konstruktor: az a függvény, ami példányosítás során lefut
+            Diak adel = new Diak();
+            adel.nev = "Adél";
+            adel.kor = 17;
+            adel.hangulat = 0.83;
+
+            Diak bela = new Diak("Béla", 15, 0.42);
+        }
+
+        static string Bemutatkozas(Tanulo t)
+        {
+            return $"{t.nev} vagyok, {t.kor} éves.";
+        }
+
+        // Procedurális programozás
+        static void Korabban()
+        {
+            Tanulo adel;
+            adel.nev = "Adél";
+            adel.kor = 17;
+            adel.hangulat = 0.83;
+
+            Tanulo bela = new Tanulo
+            {
+                nev = "Béla",
+                kor = 15,
+                hangulat = 0.42
+            };
+
+            Tanulo csaba = new Tanulo
+            {
+                nev = "Csaba",
+                kor = 18,
+                hangulat = 0.13
+            };
+
+            Console.WriteLine(Bemutatkozas(adel));
+            Console.WriteLine(Bemutatkozas(bela));
+            Console.WriteLine(Bemutatkozas(csaba));
+        }
+    }
+}
---
+++ 8-OOP/Jegyek/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jegyek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jegyek")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c8cc40f-02b2-4b69-8eb7-9a2f30e2d8b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

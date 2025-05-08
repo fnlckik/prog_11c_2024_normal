3e3f283afbf96696b146c6b9725cdc41a82a60f9
farkas.norbert <farkas.norbert@ckik.hu>
2025-05-08 16:57:49 +0200

    Filmek - adattagok, konstruktorok

---
+++ 8-OOP/Filmek/Film.cs
@@ -0,0 +1,33 @@
+﻿namespace Filmek
+{
+    class Film
+    {
+        public string cim;
+        public int ev;
+        public string mufaj;
+        public double imdb;
+        public int nezok;
+
+        public Film(string cim, int ev, string mufaj, double imdb, int nezok)
+        {
+            this.cim = cim;
+            this.ev = ev;
+            this.mufaj = mufaj;
+            this.imdb = imdb;
+            this.nezok = nezok;
+        }
+
+        // "Vérapó;2022;horror;7,5;15"
+        public Film(string szoveg)
+        {
+            string[] sor = szoveg.Split(';');
+            this.cim = sor[0];
+            this.ev = int.Parse(sor[1]);
+            this.mufaj = sor[2];
+            this.imdb = double.Parse(sor[3]);
+            this.nezok = int.Parse(sor[4]);
+        }
+
+        // public ToString
+    }
+}
---
+++ 8-OOP/Filmek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Filmek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cim, ev, mufaj, imdb, nezok
+            Film film1 = new Film("Az", 2017, "horror", 7.3, 20);
+            Film film2 = new Film("Bosszúállók", 2012, "fantasy", 8.4, 50);
+            Film film3 = new Film("Vérapó;2022;horror;7,5;15");
+            Console.WriteLine($"{film1.cim} {film2.cim} {film3.cim}");
+
+            Console.WriteLine(film1); // Az (2017) - 7,3
+            Console.WriteLine(film2); // Bosszúállók (2012) - 8,4
+            Console.WriteLine(film3); // Vérapó (2022) - 7,5
+        }
+    }
+}

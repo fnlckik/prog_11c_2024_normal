9e6a5d1ce4167e0dd734ebfe059523b08ab04b0f
farkas.norbert <farkas.norbert@ckik.hu>
2025-05-15 08:23:39 +0200

    Csoport.cs konstruktor

--- 8-OOP/Jegyek/Csoport.cs
+++ 8-OOP/Jegyek/Csoport.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Jegyek
 {
@@ -7,27 +8,32 @@ namespace Jegyek
         private int letszam;
         private List<Diak> diakok = new List<Diak>();
 
+        // Diák életkora: 14-19
+        // r.NextDouble();
         public Csoport(int n)
         {
             this.letszam = n;
+            Random r = new Random();
             for (int i = 0; i < n; i++)
             {
-                Diak d = new Diak("Tivadar", 87, 0.11);
+                int kor = r.Next(14, 20);
+                double hangulat = r.Next(0, 101) * 0.01;
+                Diak d = new Diak("Tivadar", kor, hangulat);
                 this.diakok.Add(d);
             }
         }
 
         public int Letszam { get => this.letszam; }
 
-        // Tivadar vagyok, 87 éves.
-        // Tivadar vagyok, 87 éves.
-        // Tivadar vagyok, 87 éves.
+        // Tivadar vagyok, 87 éves. Hangulatom: 11%
+        // Tivadar vagyok, 87 éves. Hangulatom: 11%
+        // Tivadar vagyok, 87 éves. Hangulatom: 11%
         public override string ToString()
         {
             string s = "";
             foreach (Diak diak in this.diakok)
             {
-                s += diak.Bemutatkozas() + "\n";
+                s += diak + $" Hangulatom: {diak.Hangulat}%\n";
             }
             return s;
         }
